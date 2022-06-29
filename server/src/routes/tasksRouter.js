fbc75d890499dc1f18a3c437a6990aa123eab829
TiLourinho <lourinho.tiago@gmail.com>
2022-06-29 20:14:02 -0300

    feat: endpoint PUT /tasks/:id

--- server/src/routes/tasksRouter.js
+++ server/src/routes/tasksRouter.js
@@ -6,5 +6,6 @@ const TaskController = require('../controllers/TaskController');
 tasksRouter.post('/', rescue(TaskController.create));
 tasksRouter.get('/', rescue(TaskController.getAll));
 tasksRouter.delete('/:id', rescue(TaskController.remove));
+tasksRouter.put('/:id', rescue(TaskController.update));
 
 module.exports = tasksRouter;
